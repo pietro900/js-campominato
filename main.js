5e761871ff2392458ffa9f80485a626ebed96090
pietro <pietro1990@hotmail.it>
2020-04-27 17:55:27 +0200

    inserito le ripetizioni

--- main.js
+++ main.js
@@ -19,28 +19,26 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-
-
+var tentativi = 0;
 //chiedo all'utente di inserire un numero tra uno e 100
 do {
     var numero_utente = parseInt(prompt("inserisci un numero tra 1 e  100"));
+    tentativi++
     //console.log("numero scelto dall'utente:" + numero_utente);
-} while (isNaN(numero_utente) || numero_utente < 1 || numero_utente > 100);
-console.log("perfetto hai inserito un numero valido. il numero scelto è:" + numero_utente)
+} while (!numero_pc.includes(numero_utente));
+console.log("il numero scelto è: " + numero_utente + " dopo " + tentativi + " tentativi")
 
 //devo controllare che il numero scelto dall'utente nn sia tra quelli scelti dal pc;
-function controllo_mine() {
-    var mine;
-    //cerco in tutto array se il numero dell'utente nn sia presente;
-    for (var i = 0; i < numero_pc.length; i++) {
-        // numero utente e numero pc sono uguali?
-        if(numero_utente == numero_pc[i]);
-        mine = true;
-    }
-    return controllo
-}
+var mine;
+//cerco in tutto array se il numero dell'utente nn sia presente;
+for (var i = 0; i < numero_pc.length; i++) {
+    // numero utente e numero pc sono uguali?
+    if(numero_utente == numero_pc[i]){
+    mine = true;
+    };
+};
 
-if (controllo_mine == true) {
+if (mine == true) {
     console.log("hai perso");
 }
 else {
